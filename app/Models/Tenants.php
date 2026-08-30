b31db2a53d3b7ca23c7e987b591acf67346afa9e
abdellatifbencheikh43-cell <EMAIL_OF_abdellatifbencheikh43-cell>
2026-08-30 20:41:25 +0200

    [Tenants] Create Factory & Seeders

--- app/Models/Tenants.php
+++ app/Models/Tenants.php
@@ -4,43 +4,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Str;
 
 class Tenants extends Model
 {
-    //
     use HasFactory;
 
     protected $fillable = [
         'name',
-        'slug' => Str::slug(),
+        'slug',
         'subscription_status',
         'trial_ends_at',
     ];
 
-
     protected $casts = [
         'trial_ends_at' => 'datetime',
-        'subscription_status' => 'enum'
     ];
 
-    public function users(){
+    public function users()
+    {
         return $this->hasMany(User::class);
     }
 
-    public function doctorProfiles(){
+    public function doctorProfiles()
+    {
         return $this->hasMany(DoctorProfiles::class);
     }
 
-    public function doctorSchedules(){
+    public function doctorSchedules()
+    {
         return $this->hasMany(DoctorSchedules::class);
     }
 
-    public function appointments(){
+    public function appointments()
+    {
         return $this->hasMany(Appointments::class);
     }
 
-        public function aiPatientIntakes()
+    public function aiPatientIntakes()
     {
         return $this->hasMany(AiPatientIntake::class);
     }
